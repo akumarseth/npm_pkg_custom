cf34908ce79006d1f0b69c810998c7f8f53b0b11
Abhishek Kumar Seth <abhishek.kumar1@tredence.com>
2022-12-04 00:59:07 +0530

    initial commit

---
+++ aks_npm-demo-pkg/index.js
@@ -0,0 +1,8 @@
+var colors = require('colors')
+
+exports.printMsg = function(colo, msg){
+    if(!color) color = 'red'
+    console.log(msg[color])
+}
+
+
---
+++ test/test.js
@@ -0,0 +1,4 @@
+var demo = require('aks_npm-demo-pkg')
+var style = require('z_sass_compiler')
+
+demo.printMsg('green')
